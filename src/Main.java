88b3711558a906fe95d5748990cfc4f1e575e422
Ninel0204 <116366686+Ninel0204@users.noreply.github.com>
2022-10-26 20:54:53 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        int [] priceList={500,1000,50, 6000};
+        long [] priceList={500,1000,50, 6000};
         SalesManager salesManager=new SalesManager(priceList);
         System.out.println(salesManager.max());
         System.out.println(salesManager.min());
